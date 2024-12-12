8426974bda76effea661f6ef2b19ef3aa6d3bbc3
100495797 <158482450+100495797@users.noreply.github.com>
2024-12-12 06:28:09 +0100

    final

--- scripts/modo_padres.js
+++ scripts/modo_padres.js
@@ -1,20 +1,14 @@
 function updateHomeBackground() {
     var modoPadres = JSON.parse(localStorage.getItem("modoPadres"));
     var inicioSection = document.querySelector(".Inicio");
-    var botonPadres = document.getElementById("EMPEZAR_PADRES");
-    var botonNinos = document.getElementById("EMPEZAR_NINOS");
 
     if (inicioSection) {
         if (modoPadres) {
             inicioSection.classList.add("modo-padres");
-            botonPadres.style.display = "block";
-            botonNinos.style.display = "none";
             updateNavbar();
         } else {
             
             inicioSection.classList.remove("modo-padres");
-            botonNinos.style.display = "block";
-            botonPadres.style.display = "none";
             updateNavbar();
         }
     } 
